866a9144cc3659e0a00357a3b4297c4f27886e78
Leandro Brito <leandroncbrito@gmail.com>
2018-01-25 11:35:50 -0200

    Test using Notification Fake

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -5,6 +5,8 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\ThreadWasUpdated;
 
 class ThreadTest extends TestCase
 {
@@ -54,6 +56,24 @@ class ThreadTest extends TestCase
         ]);
 
         $this->assertCount(1, $this->thread->replies);
+    }
+
+    /**
+     * @test
+     */
+    public function a_thread_notifies_all_registered_subscribers_when_a_reply_is_added()
+    {
+        Notification::fake();
+
+        $this->signIn()->
+            thread->
+            subscribe()
+            ->addReply([
+                'body' => 'Foobar',
+                'user_id' => 1
+        ]);
+
+        Notification::assertSentTo(auth()->user(), ThreadWasUpdated::class);
     }
 
     /**
