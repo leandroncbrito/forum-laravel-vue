58318a8839c0c2935091ba000eb5287625fcd0e4
Leandro Brito <leandroncbrito@gmail.com>
2018-01-26 15:42:35 -0200

    Refatoração através da classe Trending

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -7,9 +7,8 @@ use App\Channel;
 use App\Filters\ThreadFilters;
 use Illuminate\Http\Request;
 use App\User;
-use Carbon\Carbon;
 use App\Rules\SpamFree;
-use Illuminate\Support\Facades\Redis;
+use App\Trending;
 
 class ThreadController extends Controller
 {
@@ -34,7 +33,7 @@ class ThreadController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Channel $channel, ThreadFilters $filters)
+    public function index(Channel $channel, ThreadFilters $filters, Trending $trending)
     {
         $threads = $this->getThreads($channel, $filters);
 
@@ -42,9 +41,10 @@ class ThreadController extends Controller
             return $threads;
         }
 
-        $trending = array_map('json_decode', Redis::zrevrange('trending_threads', 0, 4));
-
-        return view('threads.index', compact('threads', 'trending'));
+        return view('threads.index', [
+            'threads' => $threads,
+            'trending' => $trending->get()
+        ]);
     }
 
     /**
@@ -88,17 +88,14 @@ class ThreadController extends Controller
      * @param  \App\Thread  $thread
      * @return \Illuminate\Http\Response
      */
-    public function show($channelId, Thread $thread)
+    public function show($channelId, Thread $thread, Trending $trending)
     {
         if (auth()->check()) {
             // Salva a visita do usuário para armazenar em cache
             auth()->user()->read($thread);
         }
 
-        Redis::zincrby('trending_threads', 1, json_encode([
-            'title' => $thread->title,
-            'path' => $thread->path()
-        ]));
+        $trending->push($thread);
 
         return view('threads.show', compact('thread'));
     }
--- tests/Feature/TrendingThreadTest.php
+++ tests/Feature/TrendingThreadTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Redis;
+use App\Trending;
 
 class TrendingThreadTest extends TestCase
 {
@@ -14,7 +15,10 @@ class TrendingThreadTest extends TestCase
     protected function setUp()
     {
         parent::setUp();
-        Redis::del('trending_threads');
+
+        $this->trending = new Trending();
+
+        $this->trending->reset();
     }
 
     /**
@@ -22,16 +26,14 @@ class TrendingThreadTest extends TestCase
      */
     public function it_increments_a_threads_score_each_time_it_is_read()
     {
-        $this->assertEmpty(0, Redis::zrevrange('trending_threads', 0, -1));
+        $this->assertEmpty($this->trending->get());
 
         $thread = create('App\Thread');
 
         $this->call('GET', $thread->path());
 
-        $trending = Redis::zrevrange('trending_threads', 0, -1);
+        $this->assertCount(1, $trending = $this->trending->get());
 
-        $this->assertCount(1, $trending);
-
-        $this->assertEquals($thread->title, json_decode($trending[0])->title);
+        $this->assertEquals($thread->title, $trending[0]->title);
     }
 }
